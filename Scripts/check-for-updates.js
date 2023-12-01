beced0d88058d9d10a94064467b0e8f52b994b84
Jordan Leich <jordanleich@gmail.com>
2023-12-01 04:28:41 -0500

    event and news alerts added

---
+++ Scripts/check-for-updates.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  checkForActiveEvent();
+  checkForRecentNews();
+});
+
+function checkForActiveEvent() {
+  // Replace the following line with your actual logic to determine if there's an active event.
+  var isEventActive = false;
+
+  if (isEventActive) {
+    // Display an alert
+    alert("There is an upcoming event! Check it out on the Events page.");
+  }
+}
+
+function checkForRecentNews() {
+  // Replace the following line with your actual logic to determine if there's a recent news post.
+  var isRecentNews = true;
+
+  if (isRecentNews) {
+    // Display an alert
+    alert("Recent news has been posted! Check it out on the News page.");
+  }
+}
