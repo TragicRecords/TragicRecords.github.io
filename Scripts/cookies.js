a456886a1a11b38bf3a78fc5b9516c6cee056dbe
Jordan Leich <jordanleich@gmail.com>
2023-12-01 03:26:01 -0500

    improved cookie look

--- Scripts/cookies.js
+++ Scripts/cookies.js
@@ -4,19 +4,34 @@ document.addEventListener("DOMContentLoaded", function () {
   const rejectCookiesBtn = document.getElementById("reject-cookies");
 
   // Check if user has already accepted cookies
-  if (localStorage.getItem("cookiesAccepted") !== "true") {
+  if (getCookie("cookiesAccepted") !== "true") {
     cookieBanner.style.display = "block";
   }
 
   // Event listener for accept cookies button
   acceptCookiesBtn.addEventListener("click", function () {
-    localStorage.setItem("cookiesAccepted", "true");
+    setCookie("cookiesAccepted", "true", 365); // Set cookie to expire in 365 days
     cookieBanner.style.display = "none";
   });
 
   // Event listener for reject cookies button
   rejectCookiesBtn.addEventListener("click", function () {
-    localStorage.setItem("cookiesAccepted", "false");
+    setCookie("cookiesAccepted", "false", 365); // Set cookie to expire in 365 days
     cookieBanner.style.display = "none";
   });
+
+  // Function to set a cookie
+  function setCookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+  }
+
+  // Function to get the value of a cookie
+  function getCookie(name) {
+    const cookieValue = document.cookie.match(
+      `(^|;)\\s*${name}\\s*=\\s*([^;]+)`
+    );
+    return cookieValue ? cookieValue.pop() : null;
+  }
 });
