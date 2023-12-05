05a09c1a94dda95a20d7cd8bf2d82a20b6d95e53
Jordan Leich <jordanleich@gmail.com>
2023-12-05 12:13:08 -0500

    u

--- Scripts/check-for-updates.js
+++ Scripts/check-for-updates.js
@@ -4,21 +4,17 @@ $(document).ready(function () {
 });
 
 function checkForActiveEvent() {
-  // Replace the following line with your actual logic to determine if there's an active event.
   var isEventActive = false;
 
   if (isEventActive) {
-    // Display an alert
     alert("There is an upcoming event! Check it out on the Events page.");
   }
 }
 
 function checkForRecentNews() {
-  // Replace the following line with your actual logic to determine if there's a recent news post.
-  var isRecentNews = true;
+  var isRecentNews = false;
 
   if (isRecentNews) {
-    // Display an alert
     alert("Recent news has been posted! Check it out on the News page.");
   }
 }
